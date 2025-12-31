cd58970b49ebeb3a1037606d4a18e8b9ae1424cf
Rafly Anggara Putra <168728388+Apewww@users.noreply.github.com>
2025-12-31 19:20:35 +0700

    Update sidebar.php

--- app/views/layout/sidebar.php
+++ app/views/layout/sidebar.php
@@ -1,14 +1,19 @@
+<?php
+$current_page = trim(parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH), '/');
+// echo($current_page);
+?>
+
 <div class="sidebar" id="sidebar">
     <div class="logo">
         <img src="logo-esteh.png" alt="Logo Es Teh">
     </div>
 
     <div class="sidebar-menu">
-        <a href="#">Produk</a>
-        <a href="#">Manajemen Barang</a>
-        <a href="#">Barang Masuk</a>
-        <a href="#" class="active">Barang Keluar</a>
-        <a href="#">Transaksi</a>
-        <a href="#">Manajemen Karyawan</a>
+        <a href="#" class="<?= ($current_page == '#') ? 'active' : '' ?>">Produk</a>
+        <a href="#" class="<?= ($current_page == '#') ? 'active' : '' ?>">Manajemen Barang</a>
+        <a href="#" class="<?= ($current_page == 'barang/cabang/masuk') ? 'active' : '' ?>">Barang Masuk</a>
+        <a href="#" class="<?= ($current_page == 'barang/cabang/keluar') ? 'active' : '' ?>">Barang Keluar</a>
+        <a href="#" class="<?= ($current_page == '#') ? 'active' : '' ?>">Transaksi</a>
+        <a href="#" class="<?= ($current_page == '#') ? 'active' : '' ?>">Manajemen Karyawan</a>
     </div>
 </div>
