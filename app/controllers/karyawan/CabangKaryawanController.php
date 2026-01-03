b252098a77e6e093d27f38e299ca5cba82fcc142
Rafly Anggara Putra <168728388+Apewww@users.noreply.github.com>
2026-01-03 12:35:26 +0700

    Refactor Code
    - Karyawan Cabang
    - Karyawan Pusat

--- app/controllers/karyawan/CabangKaryawanController.php
+++ app/controllers/karyawan/CabangKaryawanController.php
@@ -1,8 +1,19 @@
 <?php
-class CabangKaryawanController {
 
-    function index() {
-        require BASE_PATH . 'app/views/karyawan/cabang/index.php';
+require_once __DIR__ . '/../RenderViewController.php';
+
+class CabangKaryawanController {
+    private $render;
+
+    public function __construct(){
+        $this->render = new RenderViewController();
+    }
+
+    public function index() {
+        $data['title'] = 'Manajemen Karyawan';
+        $data['role'] = 'Owner';
+
+        $this->render->render('karyawan/cabang/index', $data);
     }
 
 }
--- app/controllers/karyawan/PusatKaryawanController.php
+++ app/controllers/karyawan/PusatKaryawanController.php
@@ -1,8 +1,19 @@
 <?php
-class PusatKaryawanController {
 
-    function index() {
-        require BASE_PATH . 'app/views/karyawan/pusat/index.php';
+require_once __DIR__ . '/../RenderViewController.php';
+
+class PusatKaryawanController {
+    private $render;
+
+    public function __construct() {
+        $this->render = new RenderViewController();
+    }
+
+    public function index() {
+        $data['title'] = 'Manajemen Karyawan';
+        $data['role'] = 'Karyawan';
+
+        $this->render->render('karyawan/pusat/index', $data);
     }
 
 }
