3637cccd423221293c8371c6daf146d99b44aae8
Rafly Anggara Putra <168728388+Apewww@users.noreply.github.com>
2026-01-05 11:33:02 +0700

    Update database.php

--- app/config/database.php
+++ app/config/database.php
@@ -1,8 +1,8 @@
 <?php
 
-define('DB_HOST', '103.195.65.200');
-define('DB_NAME', 'tubes_basdat');
-define('DB_USER', 'admin');
-define('DB_PASS', 'tubesbasdat!@#');
+define('DB_HOST', '');
+define('DB_NAME', '');
+define('DB_USER', '');
+define('DB_PASS', '');
 
 // define('BASE_URL', 'http://localhost/esteh-franchise-system/');
